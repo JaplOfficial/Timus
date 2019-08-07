faefa9b472bcb6226f33931c572110e36863a3ab
JaplOfficial <53877277+JaplOfficial@users.noreply.github.com>
2019-08-07 20:47:00 +0100

    Create 1225 - Flags.cpp

---
+++ 1225 - Flags.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  vector<long long int>dp(n);
+  dp[0]=2;
+  dp[1]=2;
+  for(int i=2;i<n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
+  }
+  cout<<dp[n-1]<<endl;
+}
